399483eebe4a21c187ab049807eab95e22acaab4
Rajesh Kodaganti (Rugved) <kodagantir295@gmail.com>
2025-09-11 20:12:19 -0700

    fix: add missing React imports to Parvas components

--- components/ParvasSection.tsx
+++ components/ParvasSection.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { PARVAS } from '../parvas';
 
 const ParvasSection: React.FC = () => (
--- pages/ParvasPage.tsx
+++ pages/ParvasPage.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import ParvasSection from '../components/ParvasSection';
 
 const ParvasPage: React.FC = () => (
